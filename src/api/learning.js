8fe0be52c53e8f17a7ec61a5a3761c12211f09fb
Lore Goodman <1055441588@qq.com>
2025-04-24 00:53:17 +0800

    去除一些冗余代码

--- src/api/learning.js
+++ src/api/learning.js
@@ -14,14 +14,13 @@ export const fetchLearningStats = async (userId) => {
   if (!response.ok) throw new Error('获取数据失败');
   
   const { data } = await response.json(); // 解构data字段
-  console.log("api取回时长",data)
+  console.log("api取回的学习状态",data)
   // 安全访问嵌套数据
   return {
     coursesCompleted: data?.completed_courses || 0,
     dailyStudy: Object.values(data?.daily_study_minutes || {}).reduce((a, b) => (a || 0) + (b || 0), 0),
     totalTime: Object.values(data?.history_study_minutes || {}).reduce((a, b) => (a || 0) + (b || 0), 0),
-    weeklyGoal: 600,
-    dailyGoal: 120
+
   };
 };
 
